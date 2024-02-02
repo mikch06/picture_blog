dec3ecba49f4f49b9e83221fe49bf0f8b713193b
mik <mail@kissel.ch>
2024-02-02 18:17:42 +0100

    init new main!

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
-""" import os
-import pathlib
-import shutil
+from exif import Image
 from pathlib import Path
-import markdown
-from datetime import datetime """
-from pathlib import Path
-from PIL import Image
-from PIL.ExifTags import TAGS
 
 # Filepath
 source = 'pics'
 dest = 'web'
 
+# Read pictures from source folder
 pics = Path(source).glob('*')
 
 for pic in pics:
-
-    # open the image
-    image = Image.open(pic)
-
-    # extracting the exif metadata
-    exifdata = image.getexif()
-
-    # looping through all the tags present in exifdata
-    for tagid in exifdata:
-        
-        # getting the tag name instead of tag id
-        tagname = TAGS.get(tagid, tagid)
-
-        # passing the tagid to get its respective value
-        value = exifdata.get(tagid)
-
-        # printing the final result
-        print(f"{tagname:25}: {value}")
-
-
-
-
-# # Generate timestamp
-# now = datetime.now()
-# stamp = now.strftime("%Y-%m-%d %H:%M")
+    with open(pic, 'rb') as src:
+        img = Image(src)
+        #print(img.list_all())
+        # print (src.name, img)
+        print("Picture Name: ", pic)
+        print("Picture Date: ", img.get('datetime'))
