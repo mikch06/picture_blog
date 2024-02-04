cda4ff045daaa7ebefc561c96266cd171fd31a56
mik <mail@kissel.ch>
2024-02-04 09:47:54 +0100

    Format image date from exif data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from exif import Image
 from pathlib import Path
+import time
+from datetime import datetime
 
 # Filepath
 source = 'pics'
@@ -11,10 +13,19 @@ pics = Path(source).glob('*')
 for pic in pics:
     with open(pic, 'rb') as src:
         img = Image(src)
-        #print(img.list_all())
-        # print (src.name, img)
+        print("\nHandle new picutre")
         print("Picture Name: ", pic)
         print("Picture Date: ", img.get('datetime'))
         print("----------------------------------")
+
+        # Reformat exif timestamp
+        dateFormat = "%Y-%m-%d-%M"
+        strImageDate = img.get('datetime')
+        imageDate = datetime.strptime(strImageDate,"%Y:%m:%d %H:%M:%S")
+        imageDate = datetime.strftime(imageDate, dateFormat)
+        print("Picture New Format: ", imageDate)
+
+
+
 
 print("\n\nPicture Blog handle endup.")
