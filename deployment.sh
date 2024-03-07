d9c6524f5b776573375ff277919e494ce4ad2413
mik <mail@kissel.ch>
2024-03-07 23:11:47 +0100

    add remote_images path, update rsync jobs

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# set -x
+
 clear
 echo '... Processing images'
 echo 'Insert hostname: '
@@ -8,8 +10,11 @@ echo 'Username: '
 read user
 echo $user@$hostname
 
-# remote_images='/home/mikcyons/www/wein/images'
+remote_dir='/home/mikcyons/www/wein'
 
-rsync -aPu  $user@$hostname:/home/mikcyons/www/wein/images/* images
+rsync -aPu  $user@$hostname:$remote_dir/images/* images
 
 python3 main.py
+
+rsync -aPu images/* $user@$hostname:$remote_dir/images/
+rsync -aPu index.html $user@$hostname:$remote_dir
