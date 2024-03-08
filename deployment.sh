c56cda922658967c7358d00c4bbddf228349a9c8
mik <tools@kissel.ch>
2024-03-08 15:29:18 +0100

    comments

--- deployment.sh
+++ deployment.sh
@@ -10,11 +10,14 @@ echo 'Username: '
 read user
 echo $user@$hostname
 
+# Basedir on remote host
 remote_dir='/home/mikcyons/www/wein'
 
+# Sync down from remote host
 rsync -aPu  $user@$hostname:$remote_dir/images/* images
 
 python3 main.py
 
+# Up sync all to remote host
 rsync -aPu images/* $user@$hostname:$remote_dir/images/
 rsync -aPu index.html $user@$hostname:$remote_dir
