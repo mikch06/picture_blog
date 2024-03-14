44230c4d90e223ff4691baa4d5355783d87ef74c
mik <mail@kissel.ch>
2024-03-14 22:37:24 +0100

    replace variables

--- deployment.sh
+++ deployment.sh
@@ -3,24 +3,36 @@
 # set -x
 
 clear
+# read hostname
+# read user
+
+if [ -z $1]
+then
+    echo 'Exit --> Use user and hostname as $1 and $2'
+    echo ''
+    echo ''
+    exit
+    
+fi
+
 echo '... Processing images'
-echo 'Insert hostname: '
-read hostname
-echo 'Username: '
-read user
+
 echo $user@$hostname
 
 # Basedir on remote host
 clear
-echo "Sync from remote directory"
 remote_dir='/home/mikcyons/www/wein'
 
 # Sync down from remote host
-rsync -aPu  $user@$hostname:$remote_dir/images/* images
+echo "Sync from remote directory"
+rsync -aPu  $1@$2:$remote_dir/images/* images
 
 python3 main.py
 
 # Up sync all to remote host
+echo ''
+echo ''
 echo "Sync to remote directory"
-rsync -aPu images/* $user@$hostname:$remote_dir/images/
-rsync -aPu index.html $user@$hostname:$remote_dir
+rsync -aPu images/* $1@$2:$remote_dir/images/
+rsync -aPu index.html $1@$2:$remote_dir
+echo 'All jobs done'
