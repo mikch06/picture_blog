8ff092a8ca1361a6170e46f0fc121e80555754d8
mik <mail@kissel.ch>
2024-03-14 20:51:35 +0100

    add echo exp

--- deployment.sh
+++ deployment.sh
@@ -11,6 +11,8 @@ read user
 echo $user@$hostname
 
 # Basedir on remote host
+clear
+echo "Sync from remote directory"
 remote_dir='/home/mikcyons/www/wein'
 
 # Sync down from remote host
@@ -19,5 +21,6 @@ rsync -aPu  $user@$hostname:$remote_dir/images/* images
 python3 main.py
 
 # Up sync all to remote host
+echo "Sync to remote directory"
 rsync -aPu images/* $user@$hostname:$remote_dir/images/
 rsync -aPu index.html $user@$hostname:$remote_dir
